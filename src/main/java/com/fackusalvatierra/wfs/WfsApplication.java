f6fb4d135a8fd695d611fe44c33d00d8d23298d2
Facundo <facku995@gmail.com>
2022-08-19 22:28:37 -0300

    Cambiado la pagina

--- src/main/java/com/fackusalvatierra/wfs/WfsApplication.java
+++ src/main/java/com/fackusalvatierra/wfs/WfsApplication.java
@@ -19,7 +19,7 @@ public class WfsApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://facundo-salvatierra.web.app"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://localhost:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
